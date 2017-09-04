78b9fd623116b47fc432c0cfd9c15b062ea08892
smondal <mondalda@gmail.com>
2017-09-04 11:02:48 -0700

    a test programm

---
+++ testprogram.c
@@ -0,0 +1,15 @@
+   
+    #include <stdio.h>
+    #define LAST 10
+      
+    int main()
+    {
+        int i, sum = 0;
+       
+        for ( i = 1; i <= LAST; i++ ) {
+          sum += i;
+        } /*-for-*/
+        printf("sum = %d\n", sum);
+
+        return 0;
+    }
